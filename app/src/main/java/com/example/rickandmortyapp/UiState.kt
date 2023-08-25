dc2e38b01de7bdcb9bc16ace9315a102daba1951
tahaaltuntasdev <m.tahaaltuntas@gmail.com>
2023-08-25 12:28:11 +0300

    add

---
+++ app/src/main/java/com/example/rickandmortyapp/UiState.kt
@@ -0,0 +1,9 @@
+package com.example.rickandmortyapp
+
+import com.example.rickandmortyapp.data.model.Character
+
+sealed class UiState {
+    object Loading : UiState()
+    data class Success(val data: List<Character>) : UiState()
+    data class SuccessId(val dataId: Character) : UiState()
+}
